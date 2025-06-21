ec5c33a8423836694b9ebc4979cc7226d1e7d614
SiddhBaccha <113815539+SiddhBaccha@users.noreply.github.com>
2025-06-21 15:28:33 +0530

    Add files via upload

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,1 +1,19 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCUawUr-MBbFhoIsHIFmu-kIoo7cuNsMgE",
+  authDomain: "smart-inventory-manageme-b6c39.firebaseapp.com",
+  databaseURL: "https://smart-inventory-manageme-b6c39-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "smart-inventory-manageme-b6c39",
+  storageBucket: "smart-inventory-manageme-b6c39.appspot.com",
+  messagingSenderId: "352065422117",
+  appId: "1:352065422117:web:45b7bf2a71644bf850d0cc"
+};
+
+// Prevent reinitializing on HMR
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const db = getDatabase(app);
+export const auth = getAuth(app);
