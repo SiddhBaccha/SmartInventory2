6f8966ce4d0cb0a60ebf6792cd2b7537b6d73020
SiddhBaccha <113815539+SiddhBaccha@users.noreply.github.com>
2025-06-24 19:39:06 +0530

    firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUawUr-MBbFhoIsHIFmu-kIoo7cuNsMgE",
-  authDomain: "smart-inventory-manageme-b6c39.firebaseapp.com",
-  databaseURL: "https://smart-inventory-manageme-b6c39-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "smart-inventory-manageme-b6c39",
-  storageBucket: "smart-inventory-manageme-b6c39.appspot.com",
-  messagingSenderId: "352065422117",
-  appId: "1:352065422117:web:45b7bf2a71644bf850d0cc"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Prevent reinitializing on HMR
